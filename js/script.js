c8cd18e6ad7033a760262052e4e520b3002567c5
Hubert Hadyński <hubert.hadynski@gmail.com>
2018-07-23 17:47:15 +0200

    puted getJoke as parameter

--- js/script.js
+++ js/script.js
@@ -5,18 +5,18 @@ var url = 'http://api.icndb.com/jokes/random';
 var paragraph = document.getElementById('joke');
 
 var button = document.getElementById('get-joke');
-button.addEventListener('click', function(){
-  getJoke();
+button.addEventListener('click', function(getJoke) {
+
 });
 
 function getJoke() {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.addEventListener('load', function(){
-    var response = JSON.parse(xhr.response);
-    paragraph.innerHTML = response.value.joke;
-  });
-  xhr.send();
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.addEventListener('load', function() {
+        var response = JSON.parse(xhr.response);
+        paragraph.innerHTML = response.value.joke;
+    });
+    xhr.send();
 }
 
 getJoke();
